8105bc0f8b609bb698bfd9c30dcfc901124a7b15
kofil8 <mohammedkofil8@gmail.com>
2025-11-16 08:17:10 +0600

    remove aditional panel page

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,23 @@
-import { SiteHeader } from '@/components/shared/SiteHeader'
-import { SiteFooter } from '@/components/shared/SiteFooter'
-import { FeaturedBundles } from '@/components/home/FeaturedBundles'
-import { FeaturedPanelsHome } from '@/components/home/FeaturedPanelsHome'
-import { HeroSection } from '@/components/home/HeroSection'
-import { StatsSection } from '@/components/home/StatsSection'
-import { TestimonialSection } from '@/components/home/TestimonialSection'
-import { FloatingActionButton } from '@/components/shared/FloatingActionButton'
+import { SiteHeader } from "@/components/shared/SiteHeader";
+import { SiteFooter } from "@/components/shared/SiteFooter";
+import { FeaturedBundles } from "@/components/home/FeaturedBundles";
+import { HeroSection } from "@/components/home/HeroSection";
+import { StatsSection } from "@/components/home/StatsSection";
+import { TestimonialSection } from "@/components/home/TestimonialSection";
+import { FloatingActionButton } from "@/components/shared/FloatingActionButton";
 
 export default function HomePage() {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className='flex min-h-screen flex-col'>
       <SiteHeader />
-      <main className="flex-1">
+      <main className='flex-1'>
         <HeroSection />
         <StatsSection />
         <FeaturedBundles />
-        <FeaturedPanelsHome />
         <TestimonialSection />
       </main>
       <SiteFooter />
       <FloatingActionButton />
     </div>
-  )
+  );
 }
-
